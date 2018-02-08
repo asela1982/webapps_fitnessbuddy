018acb2fcc3919da0f58fd0c5696f23c321a7587
asela1982 <asela.d82@gmail.com>
2018-02-08 02:32:42 -0600

    add error validation

--- geo.py
+++ geo.py
@@ -15,20 +15,26 @@ def getGeo(address):
                 "address" : address,
                 "key" : api_key
                 }
-
-    # request the url
-    response = requests.get(base_url+endpoint, params=params)
-
-    results = response.json()['results'][0]
-
-    def getCountry(results):
-        for result in results['address_components']:
-            if result['types'][0] == "country":
-               return result['long_name']
     
-    
-    dictGeo['lat'] = results['geometry']['location']['lat']
-    dictGeo['lng'] = results['geometry']['location']['lng']
-    dictGeo['country'] = getCountry(results)
-    
-    return dictGeo
+    try:
+        # request the url
+        response = requests.get(base_url+endpoint, params=params)
+        results = response.json()['results'][0]
+
+        def getCountry(results):
+
+                for result in results['address_components']:
+                    if result['types'][0] == "country":
+                        return result['long_name']
+                    
+        dictGeo['lat'] = results['geometry']['location']['lat']
+        dictGeo['lng'] = results['geometry']['location']['lng']
+        dictGeo['country'] = getCountry(results)
+
+        return dictGeo
+
+    except Exception as ex:
+        dictGeo['lat'] = 88.88
+        dictGeo['lng'] = 88.88
+        dictGeo['country'] = str(ex)
+        return dictGeo
